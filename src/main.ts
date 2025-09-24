8f192d25ed99f1aff00cb9549fdcf487d176f317
Aaqil khan <aaqilpro99@gmail.com>
2025-09-24 13:43:15 +0530

    Modified the backend to support vercel build and deployment

--- src/main.ts
+++ src/main.ts
@@ -3,41 +3,43 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
+const config = new DocumentBuilder()
+  .setTitle("MyCerts")
+  .setDescription('My Certs API documentation')
+  .setVersion('1.0')
+  .addBearerAuth(
+    {
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      name: 'JWT',
+      description: 'Enter JWT token',
+      in: 'header',
+    },
+    'JWT-auth',
+  )
+  .build();
 
-  const config = new DocumentBuilder()
-    .setTitle("MyCerts")
-    .setDescription('My Certs API documentation')
-    .setVersion('1.0')
-    .addBearerAuth(
-      {
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'JWT',
-        name: 'JWT',
-        description: 'Enter JWT token',
-        in: 'header',
-      },
-      'JWT-auth',
-    )
-    .build();
+// Setup the Swagger module at the '/api-docs' endpoint
+// This sets up the endpoint for the user to access the API documentation
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
 
-    
-    // Setup the Swagger module at the '/api-docs' endpoint
-    // This sets up the endpoint for the user to access the API documentation
-    async function bootstrap() {
-      const app = await NestFactory.create(AppModule);
+  // Enable CORS for production
+  app.enableCors();
 
-      //Swagger setup and configuration
-      const document = SwaggerModule.createDocument(app, config);
-      SwaggerModule.setup('docs', app, document);
+  //Swagger setup and configuration
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
-      // Global validation pipe for DTO validation
-      app.useGlobalPipes(new ValidationPipe({
-        whitelist: true,
-        forbidNonWhitelisted: true,
-        transform: true,
-        disableErrorMessages: false,
-      }));
+  // Global validation pipe for DTO validation
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+    disableErrorMessages: false,
+  }));
+
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
